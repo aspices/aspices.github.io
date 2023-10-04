4491c8ef8c0e14a1d2b3243bf34f2d92bc865487
DienNv <dien@ideabox.com.au>
2023-10-04 22:38:10 +0700

    gen_html - list, detail

--- backend/gen_html.py
+++ backend/gen_html.py
@@ -1,3 +1,5 @@
+import json
+
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 env = Environment(
@@ -5,16 +7,37 @@ env = Environment(
     autoescape=select_autoescape()
 )
 
-template = env.get_template("product-detail.html")
+product_list = json.load(open('app/product-list.json'))
 
-# template.render(**{
+
+# template = env.get_template("product-detail.html")
+#
+# out_str = template.render(**{
 #     "name": "here"
 # })
+#
+# f = open('abc.html', 'w')
+# f.write(out_str)
+# f.close()
 
-out_str = template.render(**{
-    "name": "here"
-})
+def gen_product_list():
+    template = env.get_template("product-list.html")
+    out_str = template.render(data=product_list)
 
-f = open('abc.html', 'w')
-f.write(out_str)
-f.close()
+    f = open('../products/index.html', 'w')
+    f.write(out_str)
+    f.close()
+
+
+def gen_product_detail():
+    template = env.get_template("product-detail.html")
+
+    for (k, v) in product_list.items():
+        out_str = template.render(data=v)
+        f = open('../products/{}.html'.format(v.get('key')), 'w')
+        f.write(out_str)
+        f.close()
+
+
+gen_product_list()
+gen_product_detail()
